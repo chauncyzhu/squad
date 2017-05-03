697fb390e881237281ffa874cafbf9c09b4009d5
chauncyzhu <chauncyzhu@163.com>
2017-05-03 21:36:10 +0800

    斯坦福解析，使用constituency parse将sentence解析为constituents tree，用来表示句子中的成分是怎样的

--- preprocess/stanford_parser.py
+++ preprocess/stanford_parser.py
@@ -3,7 +3,10 @@
     使用stanford parser进行解析
 """
 import os
+import numpy as np
 import utils.data_path as dp
+import nltk.tree
+from nltk import word_tokenize
 from nltk.parse import stanford
 
 #添加stanford环境变量,此处需要手动修改，jar包地址为绝对地址。
@@ -22,9 +25,9 @@ def __getLeaves(node,constituents):
     :param constituents: 属于constituency parse
     :return: constituents--注意不包括单个词
     """
-    if node.height() > 2:
+    if node.height() > 2 and (node.label() == 'NP' or 'VP'):
         # print("node:",node)
-        # print("node leaves:",node.leaves())
+        # print("node label:",node.label())
         constituents.append(node.leaves())
         for sub_node in node:
             __getLeaves(sub_node, constituents)
@@ -41,21 +44,33 @@ def getConstituents(parser,single_line):
     # 句法标注
     # what impact did the high school education movement have on the presence of skilled workers?
     # during the mass high school education movement from 1910-1940, there was an increase in skilled workers
+    single_line = single_line.replace(" ≠",'')   #注意这里有一个坑，'≠'无法和正确解析出来
     parse_line = parser.raw_parse(single_line)
     constituents = []
     print("constituency parse by stanford corenlp")
     for line in parse_line:
-        #line.draw()
+        line.draw()
         for root in line:
             #print("parse tree:", root)
             for tree in root:
                 __getLeaves(tree, constituents)
+    token_words = word_tokenize(single_line)   #是否加上本身
+    constituents.extend([list(i) for i in np.array(token_words).reshape(len(token_words),1)])
     return constituents
 
 if __name__ == '__main__':
-    pass
-    # single_line = "super bowl 50 was an american football game to determine the champion of the national football league (nfl) for the 2015 season"
-    # constituents_list = getConstituents(single_line)  #注意，这里实际上是三层嵌套列表，需要两个循环进行解析
+    # 添加stanford环境变量,此处需要手动修改，jar包地址为绝对地址。
+    os.environ['STANFORD_PARSER'] = dp.STANFORD_PARSER
+    os.environ['STANFORD_MODELS'] = dp.STANFORD_MODELS
+    # 为JAVAHOME添加环境变量
+    java_path = dp.JAVA_PATH
+    os.environ['JAVAHOME'] = java_path
+    PAESER = stanford.StanfordParser(model_path=dp.ENGLISHPCFG)
+
+    single_line = "during the mass high school education movement from 1910-1940, there was an increase in skilled workers"
+
+    constituents_list = getConstituents(PAESER,single_line)  #注意，这里实际上是三层嵌套列表，需要两个循环进行解析
+    print(constituents_list)
     # for constituent in constituents_list:
     #     line = " ".join(constituent)
     #     print("line:",line)
